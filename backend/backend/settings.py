a0147469e7808431dc266da41f6ce623a2b76af3
JemsDevOps <Jems@synkronous.com>
2026-06-05 23:06:22 +0800

    change again settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,6 +66,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
     "https://fs6-training-git-main-jemsprogs-projects.vercel.app",  
+    "https://fs6-training-jpb6iml9a-jemsprogs-projects.vercel.app"
     "https://fs6-training.vercel.app",
 ]
 
